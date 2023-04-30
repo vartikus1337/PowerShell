ff56227f2dbb6bc8447c64861e14d09d18f56b94
vartikus1337 <96585979+vartikus1337@users.noreply.github.com>
2023-05-01 00:23:36 +0300

    fix ЛОМКИ ОТ ПРОБЕЛОВ В path

--- shortcuts.ps1
+++ shortcuts.ps1
@@ -1,19 +1,26 @@
-Write-Host "Запущенное приложение или путь с .exe, 0 для выхода, ЛОМАЕТСЯ ОТ ПРОБЕЛА В НАЗВАНИЯХ ПАПКОК И ФАЙЛОВ "
+Write-Host "Запущенное приложение или путь с .exe, 0 для выхода"
 while ($true) {
     $output = Read-Host "Name or Path"
     if ($output -eq "0") {
         Write-Host "Пока!"
         break
     }
-    $path = $output -replace 'Program Files', "'Program Files'"
-    if (($path -like "*.exe") -ne $true) { # если не находит .exe, выполняется
+    if (($output -like "*.exe") -ne $true) { # если не находит .exe, выполняется
         try {$process = Get-Process $output -ea Stop}
         catch {
             Write-Host "Нет такого приложения!"
             continue
         }
-        $path = $process.Path -replace 'Program Files', "'Program Files'"
+        $arr_path = ($process.Path).Split("\")
+    }
+    else {
+        $arr_path = ($output).Split("\")
+    }
+    $path = ""
+    foreach ($elm in $arr) { 
+        if ($elm -like "* *") { $elm = "'$elm'" }
+        $path += ($elm + "\")
     }
     $shortcut = Read-Host "Shortcut"
-    Add-Content -Path $profile -value("function " + $shortcut + " {Start-Process -FilePath " + $path + " }")
+    Add-Content -Path $profile -value("function " + $shortcut + " {Start-Process -FilePath " + $path.Remove($path.Length-1,1) + " }")
 }
